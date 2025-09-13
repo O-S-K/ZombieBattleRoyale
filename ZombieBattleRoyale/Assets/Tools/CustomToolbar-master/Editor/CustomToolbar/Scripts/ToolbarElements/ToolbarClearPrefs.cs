91c42398b7d68fa12b3cb07a7b5b6b8f4cddb45e
Osk <52168556+O-S-K@users.noreply.github.com>
2025-09-13 18:23:17 +0700

    Init pack editor 3rd

---
+++ ZombieBattleRoyale/Assets/Tools/CustomToolbar-master/Editor/CustomToolbar/Scripts/ToolbarElements/ToolbarClearPrefs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityToolbarExtender;
+
+[Serializable]
+internal class ToolbarClearPrefs : BaseToolbarElement {
+	private static GUIContent clearPlayerPrefsBtn;
+
+	public override string NameInList => "[Button] Clear prefs";
+	public override int SortingGroup => 4;
+
+	public override void Init() {
+		clearPlayerPrefsBtn = EditorGUIUtility.IconContent("SaveFromPlay");
+		clearPlayerPrefsBtn.tooltip = "Clear player prefs";
+	}
+
+	protected override void OnDrawInList(Rect position) {
+
+	}
+
+	protected override void OnDrawInToolbar() {
+		if (GUILayout.Button(clearPlayerPrefsBtn, ToolbarStyles.commandButtonStyle)) 
+		{
+			System.IO.DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath);
+			foreach (FileInfo file in di.GetFiles())
+				file.Delete();
+			foreach (DirectoryInfo dir in di.GetDirectories())
+				dir.Delete(true);
+			PlayerPrefs.DeleteAll();
+			Debug.Log("Clear Player Prefs and persistent data path: " + Application.persistentDataPath);
+		}
+	}
+}
